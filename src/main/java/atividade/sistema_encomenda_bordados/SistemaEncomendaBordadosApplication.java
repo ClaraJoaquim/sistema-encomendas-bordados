bb9aa16f79cb6d9fc793aecf4a2be8d67087dd16
ClaraJoaquim <clarajpmarques@gmail.com>
2025-05-06 20:06:02 -0300

    first commit: adicionar arquivos base do projeto

---
+++ src/main/java/atividade/sistema_encomenda_bordados/SistemaEncomendaBordadosApplication.java
@@ -0,0 +1,13 @@
+package atividade.sistema_encomenda_bordados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEncomendaBordadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEncomendaBordadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/atividade/sistema_encomenda_bordados/SistemaEncomendaBordadosApplicationTests.java
@@ -0,0 +1,13 @@
+package atividade.sistema_encomenda_bordados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEncomendaBordadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
